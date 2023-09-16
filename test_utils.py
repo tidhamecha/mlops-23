f0aa9d286f33e3f1cef6aa9769c0c683f47f4179
Tejas Indulal Dhamecha <tdhamecha@microsoft.com>
2023-09-16 15:04:02 +0530

    wip for the data split debuggin

--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,4 @@
-from utils import get_hyperparameter_combinations
+from utils import get_hyperparameter_combinations, train_test_dev_split,read_digits
 
 def test_for_hparam_cominations_count():
     # a test case to check that all possible combinations of paramers are indeed generated
@@ -23,3 +23,20 @@ def test_for_hparam_cominations_values():
     expected_param_combo_2 = {'gamma': 0.01, 'C': 1}
 
     assert (expected_param_combo_1 in h_params_combinations) and (expected_param_combo_2 in h_params_combinations)
+
+
+def test_data_splitting():
+    X, y = read_digits()
+    
+    X = X[:100,:,:]
+    y = y[:100]
+    
+    test_size = .1
+    dev_size = .6
+    train_size = 1 - test_size - dev_size
+
+    X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+
+    assert (len(X_train) == 30) 
+    assert (len(X_test) == 10)
+    assert  ((len(X_dev) == 60))
