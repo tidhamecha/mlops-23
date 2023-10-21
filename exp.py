996f104bb3b35be63881c808abc0be1043ebb867
Tejas Indulal Dhamecha <tdhamecha@microsoft.com>
2023-10-21 13:18:32 +0530

    decision tree related changes

--- exp.py
+++ exp.py
@@ -16,6 +16,9 @@ from sklearn import metrics, svm
 
 from utils import preprocess_data, split_data, train_model, read_digits, predict_and_eval, train_test_dev_split, get_hyperparameter_combinations, tune_hparams
 from joblib import dump, load
+import pandas as pd
+
+num_runs  = 5
 # 1. Get the dataset
 X, y = read_digits()
 
@@ -38,31 +41,38 @@ h_params_trees_combinations = get_hyperparameter_combinations(h_params_tree)
 classifier_param_dict['tree'] = h_params_trees_combinations
 
 
-
+results = []
 test_sizes =  [0.2]
 dev_sizes  =  [0.2]
-for test_size in test_sizes:
-    for dev_size in dev_sizes:
-        train_size = 1- test_size - dev_size
-        # 3. Data splitting -- to create train and test sets                
-        X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
-        # 4. Data preprocessing
-        X_train = preprocess_data(X_train)
-        X_test = preprocess_data(X_test)
-        X_dev = preprocess_data(X_dev)
+for cur_run_i in range(num_runs):
+    
+    for test_size in test_sizes:
+        for dev_size in dev_sizes:
+            train_size = 1- test_size - dev_size
+            # 3. Data splitting -- to create train and test sets                
+            X_train, X_test, X_dev, y_train, y_test, y_dev = train_test_dev_split(X, y, test_size=test_size, dev_size=dev_size)
+            # 4. Data preprocessing
+            X_train = preprocess_data(X_train)
+            X_test = preprocess_data(X_test)
+            X_dev = preprocess_data(X_dev)
 
 
-        for model_type in classifier_param_dict:
-            current_hparams = classifier_param_dict[model_type]
-            best_hparams, best_model_path, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
-            y_dev, current_hparams, model_type)        
-        
-            # loading of model         
-            best_model = load(best_model_path) 
+            for model_type in classifier_param_dict:
+                current_hparams = classifier_param_dict[model_type]
+                best_hparams, best_model_path, best_accuracy  = tune_hparams(X_train, y_train, X_dev, 
+                y_dev, current_hparams, model_type)        
+            
+                # loading of model         
+                best_model = load(best_model_path) 
 
-            test_acc = predict_and_eval(best_model, X_test, y_test)
-            train_acc = predict_and_eval(best_model, X_train, y_train)
-            dev_acc = best_accuracy
+                test_acc = predict_and_eval(best_model, X_test, y_test)
+                train_acc = predict_and_eval(best_model, X_train, y_train)
+                dev_acc = best_accuracy
 
-            print("{}\ttest_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(model_type, test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
+                print("{}\ttest_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(model_type, test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
+                cur_run_results = {'model_type': model_type, 'run_index': cur_run_i, 'train_acc' : train_acc, 'dev_acc': dev_acc, 'test_acc': test_acc}
+                results.append(cur_run_results)
+
+print(pd.DataFrame(results).groupby('model_type').describe().T)
+                
 
