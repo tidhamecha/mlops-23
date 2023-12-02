5e4512e45c5ee7f65f3e857a47d4bde189c2727e
Tejas Indulal Dhamecha <tdhamecha@microsoft.com>
2023-12-02 15:45:09 +0530

    q4

--- api/app.py
+++ api/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request
+from joblib import dump, load
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
@@ -9,3 +12,22 @@ def hello_world():
 @app.route("/", methods=["POST"])
 def hello_world_post():    
     return {"op" : "Hello, World POST " + request.json["suffix"]}
+
+def load_model():
+    models={}
+    models['svm'] = load("./models/svm_gamma:1_C:10.joblib")
+    models['tree'] = load("./models/tree_max_depth:10.joblib")
+    models['lr'] = load("./models/lr_saga_solver:saga.joblib")
+
+    return models
+
+models = load_model()
+default_model_type = 'svm'
+model = models[default_model_type]
+
+@app.route("/predict/<model_type>", methods=["POST"])
+def predict(model_type):        
+    model_type = escape(model_type)
+    image = request.json["image"]    
+    digit = models[model_type].predict(image)    
+    return {"op" : int(digit[0])}
--- test_api.py
+++ test_api.py
@@ -1,5 +1,6 @@
 from api.app import app
-import pytest
+import pytest, json
+from utils import read_digits, preprocess_data
 
 
 def test_get_root():
@@ -13,6 +14,15 @@ def test_post_root():
     assert response.status_code == 200    
     assert response.get_json()['op'] == "Hello, World POST "+suffix
 
+def test_post_predict():
+    samples = get_samples_each_class()
+    i=0
+    X, y = read_digits()
+    sample = preprocess_data(X[y==i])[0]
+    response = app.test_client().post("/predict/svm", json={"image":[sample.tolist()]})    
+    assert response.status_code == 200    
+    assert response.get_json()['op'] == i
+        
 
 
     
